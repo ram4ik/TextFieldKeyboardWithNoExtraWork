7d609b62d67ed047382d6b5cfca82b6a229e3e49
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-23 16:42:38 +0300

    Initial Commit

---
+++ TextFieldKeyboardWithNoExtraWork/TextFieldKeyboardWithNoExtraWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldKeyboardWithNoExtraWorkApp.swift
+//  TextFieldKeyboardWithNoExtraWork
+//
+//  Created by ramil on 23.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldKeyboardWithNoExtraWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
