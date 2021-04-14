8d83158586dfabc007631479d9f87ebc9f91468f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-14 13:45:07 +0300

    dismissKeyboard

--- TextFieldKeyboardWithNoExtraWork/ContentView.swift
+++ TextFieldKeyboardWithNoExtraWork/ContentView.swift
@@ -7,9 +7,23 @@
 
 import SwiftUI
 
+extension View {
+    func dismissKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
 struct ContentView: View {
+    @State private var someText = ""
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            TextField("Type text...", text: $someText)
+                .padding()
+            Button("Submit") {
+                dismissKeyboard()
+            }
+        }
     }
 }
 
